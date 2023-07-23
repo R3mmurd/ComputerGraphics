db318a405bfdf0797f5f93098129befbd9997375
Alejandro <aledrums@gmail.com>
2023-07-23 16:39:11 -0500

    Added model loading project

---
+++ Lecture6/ModelLoading/include/PointLight.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Light.hpp>
+
+class PointLight: public Light
+{
+public:
+    PointLight() = default;
+
+    PointLight(GLfloat red, GLfloat green, GLfloat blue, GLfloat _ambient_intensity, GLfloat _diffuse_intensity,
+               GLfloat pos_x, GLfloat pos_y, GLfloat pos_z, GLfloat _a, GLfloat _b, GLfloat _c) noexcept;
+
+    ~PointLight();
+
+    void use(GLuint color_id, GLuint ambient_intensity_id, GLuint diffuse_intensity_id, GLuint position_id, GLuint a_id, GLuint b_id, GLuint c_id) const noexcept;
+
+protected:
+    glm::vec3 position{0.f, 0.f, 0.f};
+    GLfloat a; // Quadratic component in the equation
+    GLfloat b; // Linear component in the equation
+    GLfloat c; // Constant component in the equation
+};
